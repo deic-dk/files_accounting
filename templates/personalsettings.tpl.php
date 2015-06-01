456761b3af0fcbfa96bb2dcb9dbc7910d5b5edb9
ioanna <psyllost@gmail.com>
2015-06-01 16:52:58 +0200

    changes: txt file with user's daily storage is stored in user's directory. Monthly average storage is retrieved from another txt file in the same directory

--- templates/personalsettings.tpl.php
+++ templates/personalsettings.tpl.php
@@ -1,135 +1,87 @@
-<?php
-//OCP\Util::addScript('user_storage','personalsettings');
-?>
 <fieldset id='storageSettings' class='section'>
 
   <h2>Storage Use</h2>
-  <div id="chart_div" style="width: 100%; height: 100%;"><script type="text/javascript" src="https://www.google.com/jsapi"></script>
+  <div id="chart_div"><script type="text/javascript" src="https://www.google.com/jsapi"></script>
     <script type="text/javascript">
 	var arrayFromPHP = <?php
-				$user=OCP\USER::getUser ();
-                        $lines = file('/tank/data/owncloud/s141277@student.dtu.dk/data/diskUsage.txt');
+			$user=OCP\USER::getUser ();
 			$userStorage  = array();
-			$janUsage  = array();
-			$febUsage = array();
-			$marUsage = array();
-			$aprUsage = array();
-			$mayUsage = array();
-			$junUsage = array();
-			$julUsage = array();
-			$augUsage = array();
-			$septUsage = array();
-			$octUsage = array();
-			$novUsage = array();
-			$decUsage = array();	
-                        foreach ($lines as $line_num => $line) {
-                                $userRows = explode(" ", $line);
-                                if ($userRows[0] == $user) {
-					$month =  substr($userRows[1], 0, 2);
-				        $month = (int)$month;
-					switch ($month) {
-						case 01:
-							$janUsage[] = array('usage' => (int)$userRows[2], 'trash' => (int)$userRows[3]);
- 
-							$averageJan = array_sum(array_column($janUsage, 'usage')) / count(array_column($janUsage, 'usage'));
-             				                $averageJanTrash = array_sum(array_column($janUsage, 'trash')) / count(array_column($janUsage, 'trash'));
-							break;
-						case 02:
-							$febUsage[] = array('usage' => (int)$userRows[2], 'trash' => (int)$userRows[3]);
-							$averageFeb = array_sum(array_column($febUsage, 'usage')) / count(array_column($febUsage, 'usage'));
-                                			$averageFebTrash = array_sum(array_column($febUsage, 'trash')) / count(array_column($febUsage, 'trash'));
-                                                        break;
-						case 03:
-							$marUsage[] = array('usage' => (int)$userRows[2], 'trash' => (int)$userRows[3]);
-							$averageMar = array_sum(array_column($marUsage, 'usage')) / count(array_column($marUsage, 'usage'));
-                           			        $averageMarTrash = array_sum(array_column($marUsage, 'trash')) / count(array_column($marUsage, 'trash'));
-                                                        break;
-						case 04:
-							$aprUsage[] = array('usage' => (int)$userRows[2], 'trash' => (int)$userRows[3]);
- 
-							$averageApr = array_sum(array_column($aprUsage, 'usage')) / count(array_column($aprUsage, 'usage'));
-            			                        $averageAprTrash = array_sum(array_column($aprUsage, 'trash')) / count(array_column($aprUsage, 'trash'));
-                                                        break;
-						case 05:
-							$mayUsage[] = array('usage' => (int)$userRows[2], 'trash' => (int)$userRows[3]);
-							$averageMay = array_sum(array_column($mayUsage, 'usage')) / count(array_column($mayUsage, 'usage'));
-                           			        $averageMayTrash = array_sum(array_column($mayUsage, 'trash')) / count(array_column($mayUsage, 'trash'));
-                                                        break;
-						case 06:
-							$junUsage[] = array('usage' => (int)$userRows[2], 'trash' => (int)$userRows[3]);
-							$averageJun = array_sum(array_column($junUsage, 'usage')) / count(array_column($junUsage, 'usage'));
-                             			       $averageJunTrash = array_sum(array_column($junUsage, 'trash')) / count(array_column($junUsage, 'trash'));
-                                                        break;
-						case 07:
-							$julUsage[] = array('usage' => (int)$userRows[2], 'trash' => (int)$userRows[3]);
-							$averageJul = array_sum(array_column($julUsage, 'usage')) / count(array_column($julUsage, 'usage'));
-                       				         $averageJulTrash = array_sum(array_column($julUsage, 'trash')) / count(array_column($julUsage, 'trash'));
-                                                        break;
-						case 08:
-							$augUsage[] =array('usage' => (int)$userRows[2], 'trash' => (int)$userRows[3]);
-							$averageAug = array_sum(array_column($augUsage, 'usage')) / count(array_column($augUsage, 'usage'));
-            			                        $averageAugTrash = array_sum(array_column($augUsage, 'trash')) / count(array_column($augUsage, 'trash'));
-                                                        break;
-						case 09:
-							$septUsage[] = array('usage' => (int)$userRows[2], 'trash' => (int)$userRows[3]);
-							$averageSept = array_sum(array_column($septUsage, 'usage')) / count(array_column($septUsage, 'usage'));
-             			                        $averageSeptTrash = array_sum(array_column($septUsage, 'trash')) / count(array_column($septUsage, 'trash'));
-                                                        break;
-						case 10:
-							$octUsage[] = array('usage' => (int)$userRows[2], 'trash' => (int)$userRows[3]);
-							$averageOct = array_sum(array_column($octUsage, 'usage')) / count(array_column($octUsage, 'usage'));
-            				                $averageOctTrash = array_sum(array_column($octUsage, 'trash')) / count(array_column($octUsage, 'trash'));
-                                                        break;
-						case 11:
-							$novUsage[] =array('usage' => (int)$userRows[2], 'trash' => (int)$userRows[3]);
-							$averageNov = array_sum(array_column($novUsage, 'usage')) / count(array_column($novUsage, 'usage'));
-            		      	 	                $averageNovTrash = array_sum(array_column($novUsage, 'trash')) / count(array_column($novUsage, 'trash'));
-                                                        break;
-						case 12:
-							$decUsage[] = array('usage' => (int)$userRows[2], 'trash' => (int)$userRows[3]);
-							$averageDec = array_sum(array_column($decUsage, 'usage')) / count(array_column($decUsage, 'usage'));
-            				                $averageDecTrash = array_sum(array_column($decUsage, 'trash')) / count(array_column($decUsage, 'trash'));
-                                                        break;
-				}
-                                }
-                        }
-if ($averageJan != 0 ) {
-	$userStorage[] = array("Jan", $averageJan, $averageJanTrash);
-}
-if ($averageFeb != 0) {
-	$userStorage[] = array("Feb", $averageFeb, $averageFebTrash);
-} 
-if ($averageMar != 0 ) {
-	$userStorage[] = array("Mar", $averageMar, $averageMarTrash);
-}
-if ($averageApr != 0 ) {
-	$userStorage[] = array("Apr", $averageApr, $averageAprTrash);
-}
-if ($averageMay != 0) {
-	$userStorage[] = array("May", $averageMay, $averageMayTrash);
-}
-if ($averageJun != 0) {
-	$userStorage[] = array("Jun", $averageJun, $averageJunTrash);
-}
-if ($averageJul != 0 ) {
-	$userStorage[] = array("Jul", $averageJul, $averageJulTrash);
-}
-if ($averageAug != 0 ) {
-	$userStorage[] = array("Aug", $averageAug, $averageAugTrash);
-}
-if ($averageSept != 0 ) {
-	$userStorage[] = array("Sep", $averageSept, $averageSeptTrash);
-} 
-if ($averageOct != 0 ) {
-	$userStorage[] = array("Oct", $averageOct, $averageOctTrash);
-}
-if ($averageNov != 0 ) {
-	$userStorage[] = array("Nov", $averageNov, $averageNovTrash);
-}
-if ($averageDec != 0 ) {
-	$userStorage[] = array("Dec", $averageDec, $averageDecTrash);
-}
-			
+			if (file_exists('/tank/data/owncloud/'.$user.'/diskUsageAverage.txt')) {
+                          $average_lines = file('/tank/data/owncloud/'.$user.'/diskUsageAverage.txt');
+                                  foreach ($average_lines as $line_num => $line) {
+                                        $userRows = explode(" ", $line);
+                                        if ($userRows[0] == $user) {
+                                          $month =  $userRows[1];
+                                          if ($month != date('m')) {
+                                                $month = (int)$month;
+                                                $averageMonth = (int)$userRows[2];
+                                                $averageMonthTrash = (int)$userRows[3];
+                                                switch ($month) {
+                                                        case 01:
+                                                                $userStorage[] = array('Jan', $averageMonth, $averageMonthTrash);
+                                                                break;
+                                                        case 02:
+                                                                $userStorage[] = array('Feb', $averageMonth, $averageMonthTrash);
+                                                                break;
+                                                        case 03:
+                                                                $userStorage[] = array('Mar', $averageMonth, $averageMonthTrash);
+                                                                break;
+                                                        case 04:
+                                                                $userStorage[] = array('Apr', $averageMonth, $averageMonthTrash);
+                                                                break;
+                                                        case 05:
+                                                                $userStorage[] = array('May', $averageMonth, $averageMonthTrash);
+                                                                break;
+                                                        case 06:
+                                                                $userStorage[] = array('Jun', $averageMonth, $averageMonthTrash);
+                                                                break;
+                                                        case 07:
+                                                                $userStorage[] = array('Jul', $averageMonth, $averageMonthTrash);
+                                                                break;
+                                                        case 08:
+                                                                $userStorage[] = array('Aug', $averageMonth, $averageMonthTrash);
+                                                                break;
+                                                        case 09:
+                                                                $userStorage[] = array('Sep', $averageMonth, $averageMonthTrash);
+                                                                break;
+                                                        case 10:
+                                                                $userStorage[] = array('Oct', $averageMonth, $averageMonthTrash);
+                                                                break;
+                                                        case 11:
+                                                                $userStorage[] = array('Nov', $averageMonth, $averageMonthTrash);
+                                                                break;
+                                                        case 12:
+                                                                $userStorage[] = array('Dec', $averageMonth, $averageMonthTrash);
+                                                                break;
+                                                }
+
+                                          }
+
+                                    }
+
+                                  }
+                    }
+
+            $lines = file('/tank/data/owncloud/'.$user.'/diskUsageDaily.txt');
+			$dailyUsage = array();
+			$averageToday = 0 ;
+			$averageTodayTrash = 0;
+            foreach ($lines as $line_num => $line) {
+                    $userRows = explode(" ", $line);
+                    if ($userRows[0] == $user) {
+						$month =  substr($userRows[1], 0, 2);
+						if ($month == date('m')) { 
+				           $month = (int)$month;
+						   $dailyUsage[] = array('usage' => (int)$userRows[2], 'trash' => (int)$userRows[3], 'month' => $month);
+						   $averageToday = array_sum(array_column($dailyUsage, 'usage')) / count(array_column($dailyUsage, 'usage'));
+						   $averageTodayTrash = array_sum(array_column($dailyUsage, 'trash')) / count(array_column($dailyUsage, 'trash'));	
+						}
+					}
+           }
+			if ($averageToday != 0 ) {
+			  $userStorage[] = array(date('M'), $averageToday, $averageTodayTrash);
+			}	
+
                         echo json_encode($userStorage);
                         ?>;
       google.load("visualization", "1", {packages:["corechart"]});
@@ -170,7 +122,6 @@ if ($averageDec != 0 ) {
                          for(i = 0; i < dates.length; i++){
                                 data.addRow([dates[i], usage[i]/1000, trash[i]/1000]);
                         }
-
                         var options = {
                         title: 'Average Storage History',
                         hAxis: {title: 'Months',  titleTextStyle: {color: '#333'}},
@@ -195,3 +146,4 @@ if ($averageDec != 0 ) {
     </script>
 </div>
 </fieldset>
+
