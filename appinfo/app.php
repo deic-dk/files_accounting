d719c24dbfb786760693649184ca14f8803a0d91
psyllost <psyllost@gmail.com>
2015-06-16 18:11:25 +0200

    Update app.php

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,10 +1,6 @@
 <?php
+
 OCP\App::checkAppEnabled('files_accounting');
-<<<<<<< HEAD
 OCP\App::registerAdmin('files_accounting', 'settings');
 OCP\Backgroundjob::registerJob('\OCA\Files_Accounting\Stats');
 
-=======
-//OCP\App::registerPersonal('files_accounting', 'personalsettings');
-OCP\Backgroundjob::registerJob('\OCA\Files_Accounting\Stats');
->>>>>>> 6dc0038988d3f84334fa5ba04473892bb8eb2d2c
