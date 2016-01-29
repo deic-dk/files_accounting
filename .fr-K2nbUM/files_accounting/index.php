5b6ee2b45e3c451f121f392e2e0543397d087b15
¨psyllost¨ <psyllost@gmail.com>
2016-01-29 17:17:34 +0100

    correct commit

--- .fr-K2nbUM/files_accounting/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-OCP\User::checkLoggedIn();
-OCP\App::checkAppEnabled('files_accounting');
-
-OCP\App::setActiveNavigationEntry( 'files_accounting' );
-
--- .fr-K2nbUM/files_accounting/settings.php
+++
@@ -1,5 +0,0 @@
-<?php
-OC_Util::checkAdminUser();
-OCP\Util::addScript('files_accounting', 'settings');
-$tmpl = new OCP\Template( 'files_accounting', 'settings.tpl');
-return $tmpl->fetchPage();
