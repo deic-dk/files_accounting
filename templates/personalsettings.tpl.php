0cd84911bade01d72a5da9a6578429fab5baaeba
ioanna <psyllost@gmail.com>
2016-03-18 18:20:07 +0100

    correction for the broken gif

--- templates/personalsettings.tpl.php
+++ templates/personalsettings.tpl.php
@@ -16,13 +16,14 @@
  >Daily History</label>
 </div>
 <div style="margin-top: 2%">
-<form action="https://www.paypalobjects.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="EPP2VQRP5XWF2">
 <table>
 <tr><td>Enter the maximum amount you want to pay each month</td></tr><tr><td><input type="text" name="max_amount" value="">
 DKK</td></tr></table>
-<table><tr><td align=center><i>Sign up for</i></td></tr><tr><td><input type="image" src="https://www.sandbox.paypal.com/en_US/GB/i/btn/btn_auto_billing_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online."></td></tr></table>
+<table><tr><td align=center><i>Sign up for</i></td></tr><tr><td><input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_auto_billing_LG.gif" border="0" name="submit" alt="PayPal \xe2\x80\
+x93 The safer, easier way to pay online."></td></tr></table>
 <img alt="" border="0" src="https://www.sandbox.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1">
 </form>
 
